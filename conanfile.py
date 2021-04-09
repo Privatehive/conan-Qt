2c3598958d6f7aced2be5f7c02a8f7df8889eeb5
Björn Stresing <bjoern.stresing@gmx.de>
2021-04-09 19:51:47 +0200

    Trying to find a workaround for Android build on msys2 subsystem

--- conanfile.py
+++ conanfile.py
@@ -340,6 +340,8 @@ class QtConan(ConanFile):
             self.run(self._toUnixPath("%s/qt5/configure " % self.source_folder) + " ".join(args), win_bash=tools.os_info.is_windows)
             if tools.os_info.is_windows:
                 self.run("make", ignore_errors=True, win_bash=tools.os_info.is_windows) # Workaround MSYS2 qmltyperegistrar.exe: Bad address
+                self.run("make", ignore_errors=True, win_bash=tools.os_info.is_windows)
+                self.run("make", ignore_errors=True, win_bash=tools.os_info.is_windows)
             self.run("make", win_bash=tools.os_info.is_windows)
             self.run("make install", win_bash=tools.os_info.is_windows)
 
